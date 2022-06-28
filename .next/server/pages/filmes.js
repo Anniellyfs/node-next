f0dc0feae37d17e33ea04bd6fd75e4aff251b357
Anniellyfs <annyfs2015@gmail.com>
2022-06-28 15:55:35 -0300

    receita-next-02

---
+++ .next/server/pages/filmes.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/filmes";
+exports.ids = ["pages/filmes"];
+exports.modules = {
+
+/***/ "./pages/filmes.js":
+/*!*************************!*\
+  !*** ./pages/filmes.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Filmes)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Filmes() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Pagina de filmes\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\ADM\\\\next0\\\\pages\\\\filmes.js\",\n        lineNumber: 2,\n        columnNumber: 12\n    }, this);\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9maWxtZXMuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFBZSxTQUFTQSxNQUFNLEdBQUU7SUFDNUIscUJBQU8sOERBQUNDLElBQUU7a0JBQUMsa0JBQWdCOzs7OztZQUFLO0NBQ25DIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbmV4dDAvLi9wYWdlcy9maWxtZXMuanM/YjU4OSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBGaWxtZXMoKXtcclxuICAgIHJldHVybiA8aDE+UGFnaW5hIGRlIGZpbG1lczwvaDE+XHJcbn0iXSwibmFtZXMiOlsiRmlsbWVzIiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/filmes.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/filmes.js"));
+module.exports = __webpack_exports__;
+
+})();
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b360934a28bf6cd2"; }
+/******/ 		__webpack_require__.h = function() { return "77e763af8435451b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
